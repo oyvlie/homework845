5f32fafc7c3b7f4a2de80412963cbad455dc706b
oyvlie <oyvind.lie@gmail.com>
2024-02-26 20:36:35 +0100

    enable aws s3 storage

--- save_json.js
+++ save_json.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
-const save = (favNumber) => {
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+  await s3
+    .putObject({
+      Body: JSON.stringify(favNumber, null, 2),
+      Bucket: "cyclic-amused-hare-cowboy-boots-eu-north-1",
+      Key: "number.json",
+    })
+    .promise();
 };
 
 module.exports = { save };
